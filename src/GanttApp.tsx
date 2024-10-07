d130f83825d2380f181e5c679dac995d12f09934
Jimmy Thomsen <jit@projectflow.dk>
2024-10-07 13:49:37 +0200

    Fixed 8hrs calendar and expanded calendar range to 10 years.

--- src/GanttApp.tsx
+++ src/GanttApp.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import React, { Fragment, FunctionComponent, useState } from 'react';
+import React, { Fragment, FunctionComponent, useEffect, useState } from 'react';
 
 import { BryntumGantt } from '@bryntum/gantt-react';
 import { addCalendar, getGanttConfig } from './GanttConfig';
@@ -9,22 +9,20 @@ import '@bryntum/gantt/gantt.classic.css';
 
 const GanttApp: FunctionComponent = () => {
     const [isTrue, setIsTrue] = useState(false);
+    const ganttRef = React.createRef<BryntumGantt>();
+
+    useEffect(() => {
+      if (ganttRef.current?.instance) {
+        addCalendar(ganttRef.current.instance);
+      }
+    }, [ganttRef]);
 
     return (
         <Fragment>
             <div className='AppPanel'>
               <button onClick={() => { setIsTrue(!isTrue); }} style={{ width: "10em" }}>Re-render</button>
             </div>
-            <BryntumGantt key={isTrue + ""} {...getGanttConfig()} ref={(ref) => {
-
-                // Calendar information is loaded async - simulate this
-                // behaviour by applying calendar once gantt is created.
-
-                (window as any).gantt = ref;
-                if (ref?.instance) {
-                  addCalendar(ref.instance)
-                }
-            }} />
+            <BryntumGantt key={isTrue + ""} {...getGanttConfig()} ref={ganttRef} />
         </Fragment>
     );
 };
